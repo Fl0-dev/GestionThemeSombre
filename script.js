9651e5a2a6fad0873ac81270a409b9ade10bed4b
Fl0-dev <florian.girard.dev@outlook.fr>
2021-07-23 11:42:06 +0200

    Add listener sur le bouton

---
+++ script.js
@@ -0,0 +1,17 @@
+//récupération des éléments
+var btn = document.querySelector('#mode');
+var span = document.querySelector('span');
+
+//fonction du bouton
+btn.addEventListener('click',()=>{
+    //récupération des class du body
+    var classesDuBody = document.body.classList;
+    //si dark on change le thème
+    if(classesDuBody.contains('dark')){
+        document.body.classList.remove('dark');
+        span.textContent ="Thème sombre";
+        localStorage.setItem('theme','clair');
+    }else{
+        modeSombre();
+    }
+})
